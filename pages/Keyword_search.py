0db059ee15b68e730eef1f460264a92a0350888e
Erin Wolfe <edw@ku.edu>
2022-10-20 14:38:08 -0500

    update keyword search

--- pages/Keyword_search.py
+++ pages/Keyword_search.py
@@ -1,5 +1,6 @@
 import streamlit as st
 state = st.session_state
+from natsort import natsorted
 from scripts import tools, getdata, kwsearchproc
 tools.page_config()
 
@@ -16,10 +17,8 @@ placeholder = st.empty()
 st.subheader('Search for keywords')
 getdata.df_summary_header()
 
-#tokenizer = state.tokenizer
-
-# user-driven search
-placeholder.markdown('*. . . Analyzing search terms . . .*\n\n')
+# keyword search
+# placeholder.markdown('*. . . Analyzing search terms . . .*\n\n')
 
 st.write('View the frequency of one or more keywords over time.')
 
@@ -27,47 +26,63 @@ kw_cols = st.columns(2)
 with kw_cols[0]:
     searchterm = st.text_input("Enter keyword(s), separated by a comma", value='test')
     omit = st.text_input("Words to omit from the results, separated by a comma (no quotes)")
-    #kwmethod_btn = st.radio('', ['Partial match','Exact match'], key='kwmethod', horizontal=True)
 
 with kw_cols[1]:
     with st.expander('Tips on keyword searching'):
         kwsearchproc.searchtips()
 
-kw_abs, kw_norm, kw_df = kwsearchproc.plot_terms_by_month(df, searchterm)
-
+# combined counts
+stlist = []
 if searchterm != '':
+    searchterm = [t.strip().lower() for t in searchterm.split(',')]
+    st.write("***Search terms included in results:***")
+    for t in searchterm:
+        if '*' in t:
+            matchdict = kwsearchproc.get_pattern(f'^{t}', ' '.join(df.clean_text), omit)
+        else:
+            matchdict = kwsearchproc.get_pattern(t, ' '.join(df.clean_text), omit)
+        # matchstr = ', '.join(natsorted([f'{k} ({v:,})' for k,v in matchdict.items()]))
+        matchstr = ', '.join(natsorted([k for k,v in matchdict.items() if kwsearchproc.kwd_count(k, ' '.join(df.clean_text), omit) > 0]))
+
+        st.write(f"**{t.strip()}** -- {matchstr}")
+
+        if t.startswith('^'):
+            stlist.extend(natsorted([k for k,v in matchdict.items() if k in matchstr]))
+        else:
+            stlist.extend([t])
+
+    st.write('### Keyword frequency')
+
+    kw_abs, kw_norm, kw_df = kwsearchproc.plot_terms_by_month(df, stlist, omit)
     kw_tab1, kw_tab2, kw_tab3 = st.tabs(["Raw count (graph)", "Normalized (graph)", "Table"])
 
     with kw_tab1:
-        st.write('### Raw count of keyword frequency over time')
-        st.write('**All sources combined**')
+        st.write('Raw count of keyword frequency over time (all sources)')
         st.plotly_chart(kw_abs, use_container_width=True)
 
     with kw_tab2:
-        st.write('### Keyword frequency over time, normalized relative to the publication frequency (scale of 0 to 1).')
-        st.write('**All sources combined**')
+        st.write('Keyword frequency over time, normalized relative to the publication frequency (scale of 0 to 1) (all sources)')
         st.plotly_chart(kw_norm, use_container_width=True)
 
     with kw_tab3:
         st.write('Raw and normalized (scale of 0 to 1) frequency counts for each term.')
         st.table(kw_df)
 
-        fn = '_'.join([t.lower().strip() for t in searchterm.split(',')])
-        st.download_button(label="Download data as CSV", data=kw_df.to_csv().encode('utf-8'),
-             file_name=f'keywords-{fn}.csv', mime='text/csv')
+        # fn = '_'.join([t.lower().strip() for t in searchterm.split(',')])
+        # st.download_button(label="Download data as CSV", data=kw_df.to_csv().encode('utf-8'),
+        #      file_name=f'keywords-{fn}.csv', mime='text/csv')
 
+    # counts by source
     if len(df.source.unique()) > 1:
 
         with st.expander('View plots by individual sources'):
 
             # plot all terms
-            terms = [t.strip() for t in searchterm.split(',')]
-            kwsearchproc.get_tabs(df, searchterm, terms)
-
-            if len(terms) > 1:
+            kwsearchproc.get_tabs(df, stlist, omit)
+            if len(stlist) > 1:
 
                 # plot individual terms
-                for term in terms:
-                    kwsearchproc.get_tabs(df, term, terms)
+                for term in stlist:
+                    kwsearchproc.get_tabs(df, term, omit)
 
 placeholder.empty()
--- scripts/kwsearchproc.py
+++ scripts/kwsearchproc.py
@@ -2,6 +2,7 @@ import streamlit as st
 state = st.session_state
 import pandas as pd
 import numpy as np
+import re
 from datetime import datetime
 from natsort import natsorted
 import plotly.graph_objs as go
@@ -24,14 +25,14 @@ def searchtips():
 * \*test\* - a single count for all matches containing the characters 'test' (e.g., 'test', 'detesting')
 * ^\*test\* - individual counts for each match containing the characters 'test'
 
-*You may find that partial matches return unwanted results. If so, enter these terms into the 'Words to omit' box and try the search again.*""")
+*You may find that wildcard search return unwanted results. If so, enter these terms into the 'Words to omit' box and try the search again.*""")
 
 def get_pattern(term, fulltext, omit):
 
     # formatting
-    for o in omit:
-        fulltext = fulltext.replace(o,'')
-    tokens = fulltext.split()
+    for o in omit.split(','):
+        fulltext = fulltext.replace(o.lower().strip(),'')
+    tokens = [ft.strip() for ft in fulltext.split()]
     term = term.strip().lower()
     t = term
     rep = ['*','^','.',',','”','"']
@@ -49,74 +50,128 @@ def get_pattern(term, fulltext, omit):
         matches = re.findall(t, fulltext)
         # return multiple matches
         if return_all:
-            wordlist = {tm:tokens.count(tm) for tm in set([w for w in tokens if t in w])}
-            return wordlist
+            matchdict = {tm:tokens.count(tm) for tm in set([w for w in tokens if t in w])}
+            return matchdict
     elif term.endswith('*'):
         matches = re.findall(fr'\b{t}', fulltext)
         # return multiple matches
         if return_all:
-            wordlist = {tm:tokens.count(tm) for tm in set([w for w in tokens if w.startswith(t)])}
-            return wordlist
+            matchdict = {tm:tokens.count(tm) for tm in set([w for w in tokens if w.startswith(t)])}
+            return matchdict
     elif term.startswith('*'):
         matches = re.findall(fr'{t}\b', fulltext)
         # return multiple matches
         if return_all:
-            wordlist = {tm:tokens.count(tm) for tm in set([w for w in tokens if w.endswith(t)])}
-            return wordlist
+            matchdict = {tm:tokens.count(tm) for tm in set([w for w in tokens if w.endswith(t)])}
+            return matchdict
     # exact count (single word or phrase)
     else:
         matches = re.findall(fr'\b{t}\b', fulltext)
 
-    wordlist = {t:len(matches)}
+    matchdict = {t:len(matches)}
+    return matchdict
 
-    return wordlist
+def kwd_count(term, fulltext, omit):
+
+    # formatting
+    for o in omit.split(','):
+        fulltext = fulltext.replace(o.lower().strip(),'')
+    tokens = [ft.strip() for ft in fulltext.split()]
+    term = term.strip().lower()
+    t = term
+    rep = ['*','^','.',',','”','"']
+    for r in rep:
+        t = t.replace(r,'')
+
+    # partial matches
+    if term.startswith('*') and term.endswith('*'):
+        matches = re.findall(t, fulltext)
+    elif term.endswith('*'):
+        matches = re.findall(fr'\b{t}', fulltext)
+    elif term.startswith('*'):
+        matches = re.findall(fr'{t}\b', fulltext)
+    # exact count (single word or phrase)
+    else:
+        matches = re.findall(fr'\b{t}\b', fulltext)
+
+    return len(matches)
+
+def sort_legend(legend_ct, fig):
+
+    new_order = [f'{k} ({v:,} total uses)' for k,v in natsorted(legend_ct.items(), key=lambda x:x[1])]
+    ordered_legend = []
+    for i in new_order:
+        item = [t for t in fig.data if t['name'] == i]
+        ordered_legend += item
+    return ordered_legend
 
 # plot a single term across multiple sources
 @st.experimental_memo
-def plot_term_by_source(df, searchterm):
+def plot_term_by_source(df, term, omit):
 
     fig_abs = go.Figure()
     fig_norm = go.Figure()
+    df = df[~(df.clean_text.isnull())]
+    legend_ct = {}
 
     for source in df.source.unique():
 
         d_df = df[df.source==source].groupby('cleandate')
         num_articles = len(df[df.source==source])
 
-        kwsearch = [sum(' '.join(g.lemmas).split().count(term.strip()) for term in searchterm.split(',')) for n,g in d_df]
-        kwsearch_norm = ([t/num_articles for t in kwsearch])
-        kwcount = "{:,d}".format(sum(kwsearch))
+        if type(term) == list:
+            kwsearch = [sum(kwd_count(t, ' '.join(g.clean_text), omit) for t in term) for n,g in d_df]
+            kwsearch_norm = ([t/num_articles for t in kwsearch])
+            total_uses = sum(kwsearch)
+        else:
+
+            kwsearch = [kwd_count(term, ' '.join(g.clean_text), omit) for n,g in d_df]
+            kwsearch_norm = ([t/num_articles for t in kwsearch])
+            total_uses = sum(kwsearch)
+
+        if all(i==0 for i in kwsearch):
+            continue
 
         fig_abs.add_trace(go.Scatter(x=[n for n,g in d_df], y=kwsearch,
-                        name=f'{source} ({kwcount} total uses)', line_shape='spline', mode='lines+markers', connectgaps=True,
+                        name=f'{source} ({total_uses:,} total uses)', line_shape='spline', mode='lines+markers', connectgaps=True,
                         marker_color=(state.colors[list(df.source.unique()).index(source)])
                         ))
 
         fig_norm.add_trace(go.Scatter(x=[n for n,g in d_df], y=kwsearch_norm,
-                        name=f'{source} ({kwcount} total uses)', line_shape='spline', mode='lines+markers', connectgaps=True,
+                        name=f'{source} ({total_uses:,} total uses)', line_shape='spline', mode='lines+markers', connectgaps=True,
                         marker_color=(state.colors[list(df.source.unique()).index(source)])
                         ))
+        legend_ct[source] = total_uses
+
+    fig_abs.data = sort_legend(legend_ct, fig_abs)
+    fig_abs.update_layout(showlegend=True, legend={'traceorder':'reversed'})
+
+    fig_norm.data = sort_legend(legend_ct, fig_norm)
+    fig_norm.update_layout(showlegend=True, legend={'traceorder':'reversed'})
 
     return fig_abs, fig_norm, None
 
 # plot multiple terms over time
 @st.experimental_memo
-def plot_terms_by_month(df, searchterm):
+def plot_terms_by_month(df, stlist, omit):
 
     fig_abs = go.Figure()
     fig_norm = go.Figure()
 
-    d_df = df[~(df.clean_text.isnull()) & ~(df.lemmas.isnull())].groupby('cleandate')
+    d_df = df[~(df.clean_text.isnull())].groupby('cleandate')
     timeframe = [getdata.get_cleandate(g.iloc[0].date) for n,g in d_df]
     kw_df = pd.DataFrame({'date':timeframe})
+    legend_ct = {}
     ct = 0
 
-    for term in searchterm.split(','):
+    for term in stlist:
 
-        total_uses = ' '.join(df.clean_text).count(term)
+        total_uses = kwd_count(term, ' '.join(df.clean_text), omit)
+        if total_uses == 0:
+            continue
 
-        kwsearch_abs = [' '.join(g.clean_text).count(term) for n,g in d_df]
-        kwsearch_norm = [' '.join(g.clean_text).count(term)/len(g) for n,g in d_df]
+        kwsearch_abs = [kwd_count(term, ' '.join(g.clean_text), omit) for n,g in d_df]
+        kwsearch_norm = [kwd_count(term, ' '.join(g.clean_text), omit)/len(g) for n,g in d_df]
         # normalize to data
         kwsearch_norm = minmax_scale(kwsearch_norm)
 
@@ -124,29 +179,41 @@ def plot_terms_by_month(df, searchterm):
         fig_abs.add_trace(go.Scatter(x=timeframe, y=kwsearch_abs,
                         name=f'{term} ({total_uses:,} total uses)', text=kwsearch_abs,
                         mode='lines+markers', connectgaps= True,
-                        line_shape='spline', marker_color=(state.colors[ct])))
+                        line_shape='spline')) # , marker_color=(state.colors[ct])
         fig_norm.add_trace(go.Scatter(x=timeframe, y=kwsearch_norm,
                         name=f'{term} ({total_uses:,} total uses)', text=kwsearch_norm,
                         mode='lines+markers', connectgaps= True,
-                        line_shape='spline', marker_color=(state.colors[ct])))
+                        line_shape='spline')) # , marker_color=(state.colors[ct])
         # add to df
         kw_df[f'{term} - Raw count'] = kwsearch_abs
         kw_df[f'{term} - Normalized'] = kwsearch_norm
+        legend_ct[term] = total_uses
 
         ct += 1
-    fig_abs.update_layout(showlegend=True)
-    fig_norm.update_layout(showlegend=True)
+
+    fig_abs.data = sort_legend(legend_ct, fig_abs)
+    fig_abs.update_layout(showlegend=True, legend={'traceorder':'reversed'})
+
+    fig_norm.data = sort_legend(legend_ct, fig_norm)
+    fig_norm.update_layout(showlegend=True, legend={'traceorder':'reversed'})
 
     return fig_abs, fig_norm, kw_df
 
-def get_tabs(df_filtered, term, terms):
+def get_tabs(df, term, omit):
 
-    if len(term.split(',')) > 1:
-        st.write(f"#### Keywords - all ({' + '.join(terms)})")
+    try:
+        if kwd_count(term, ' '.join(df.clean_text), omit) == 0:
+            return
+    except AttributeError:
+        pass
+
+    if type(term) == list:
+        st.write(f"#### Keywords - all")
+        st.write(f'{" + ".join(term)}')
     else:
         st.write(f"#### Keyword - {term}")
 
-    kw_src_abs_t, kw_src_norm_t, kw_src_df = plot_term_by_source(df_filtered, term)
+    kw_src_abs_t, kw_src_norm_t, kw_src_df = plot_term_by_source(df, term, omit)
     t1, t2, t3 = st.tabs(["Raw count (graph)", "Normalized (graph)", "Table"])
 
     with t1:
@@ -161,6 +228,6 @@ def get_tabs(df_filtered, term, terms):
         st.write('Raw and normalized (scale of 0 to 1) frequency counts for each term.')
         st.table(kw_src_df)
 
-        fn = '_'.join([t.lower().strip() for t in terms])
+        # fn = '_'.join([t.lower().strip() for t in terms])
         # st.download_button(label="Download data as CSV", data=kw_src_df.to_csv().encode('utf-8'),
         #      file_name=f'keywords-{fn}.csv', mime='text/csv', key=f'kw_dl{terms.index(term)}')
